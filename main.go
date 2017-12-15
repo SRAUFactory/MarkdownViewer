c05332ea2e4daa65d19125316acc2c9276f533be
SRAUFactory <srau.factory@gmail.com>
2017-12-15 21:24:37 +0900

    Display markdown data to browser

--- main.go
+++ main.go
@@ -3,12 +3,18 @@ package main
 import "fmt"
 import "github.com/russross/blackfriday"
 import "io/ioutil"
+import "net/http"
 
-func main() {
+func view(w http.ResponseWriter, req *http.Request) {
 	md, err := ioutil.ReadFile("test.md")
 	if err != nil {
 		panic(err)
 	}
 	output := blackfriday.MarkdownCommon([]byte(md))
-	fmt.Println(string(output))
+	fmt.Fprintf(w, string(output))
+}
+
+func main() {
+	http.HandleFunc("/", view)
+	http.ListenAndServe(":8080", nil)
 }
