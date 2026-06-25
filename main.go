5107e9f8dd26fa8243e6fff82b3f5be8e0c355a1
SRAUFactory <srau.factory@gmail.com>
2026-06-25 15:56:32 +0900

    Update main.go

--- main.go
+++ main.go
@@ -16,26 +16,30 @@ func view(w http.ResponseWriter, r *http.Request) {
 		name = "index"
 	}
 
-	home := os.Getenv("MARK_DOWN_HOME")
-	if home == "" {
-		home = "."
-	}
-	filePath := filepath.Join(home, name+".md")
+	var htmlContent template.HTML
+	if name != "index" {
+		home := os.Getenv("MARK_DOWN_HOME")
+		if home == "" {
+			home = "."
+		}
+		filePath := filepath.Join(home, name+".md")
 
-	md, err := os.ReadFile(filePath)
-	if err != nil {
-		http.Redirect(w, r, "/", http.StatusFound)
-		return
-	}
+		md, err := os.ReadFile(filePath)
+		if err != nil {
+			http.Redirect(w, r, "/", http.StatusFound)
+			return
+		}
 
-	output := blackfriday.MarkdownCommon(md)
+		output := blackfriday.MarkdownCommon(md)
+		htmlContent = template.HTML(string(output))
+	}
 
 	t, err := template.ParseFiles("index.html")
 	if err != nil {
 		http.Error(w, "Template error", http.StatusInternalServerError)
 		return
 	}
-	t.Execute(w, template.HTML(string(output)))
+	t.Execute(w, htmlContent)
 }
 
 func main() {
@@ -45,7 +49,7 @@ func main() {
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8888"
+		port = "8080"
 	}
 
 	log.Printf("Server listening on port %s...", port)
